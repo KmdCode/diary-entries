d677e8de5a729aa6ae6d9d580886866efa6a70f7
Jacob Tshepo <karabomodise260@gmail.com>
2023-05-05 18:25:34 +0200

    navbar done

---
+++ navigation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="header" id = "#home">
+        <div class="logo">
+            <h1>DIARY<span>ENTRIES</span></h1>
+        </div>
+        <div class="navbar">
+            <a href="#">FEED</a>
+            <a href="#">MY-ENTRIES</a>
+            <a href="#">FRIENDS</a>
+            <a href="#">PROFILE</a>
+            <a href="#">LOGOUT</a>
+        </div>
+    </div>
+    
+</body>
+</html>
