4b84319c387418645955a0e6e7cb8a201c8a0269
Jacob Tshepo <karabomodise260@gmail.com>
2023-05-05 17:21:29 +0200

    sign-up design done

---
+++ sign-up.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="landing">
+        
+        <h2>DIARY<span>ENTRIES</span></h2>
+        <div class="entry">
+            <div class="landing-heading">
+            <h3> SIGN UP </h3>
+            </div>
+            <div class="form" method = "post">
+                <div><input type="text" name = "username" ><label>username</label> </div>
+               <div><input type="email" name = "email" ><label>email</label></div>
+               <div><input type="password" name = "password" ><label>password</label></div>
+               <div><input type="password" name = "verify"><label>confirm password</label></div>
+                <button>Sign Up</button>
+                <div class = "ref"><a href="#">Already have an account?Sign in.</a></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
