ada1e16d9e15613f6bb6cc2f509e99115b48eb2d
Jacob Tshepo <karabomodise260@gmail.com>
2023-05-06 14:51:53 +0200

    images added

--- feed.php
+++ feed.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FEED</title>
+    <link rel="stylesheet" href="css/style.css">
     
 </head>
 <body>
     <?php require_once "navigation.php";?>
 
-    
+    <div class="slider" id="slide">
+        <h1>header</h1>
+    </div>
 
 </body>
 </html>
