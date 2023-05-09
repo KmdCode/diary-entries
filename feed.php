58118c863b0cfceec22ada22e1756d47777cd4fb
Jacob Tshepo <karabomodise260@gmail.com>
2023-05-09 17:47:32 +0200

    banner done

--- feed.php
+++ feed.php
@@ -11,6 +11,19 @@
 <body>
     <?php require_once "navigation.php";?>
     <div class="banner" id="home">
+        
+        <div class="main">
+            <h1>"It is never too late to be what you might have been." – George</h1>
+        <svg viewBox="0 0 500 500">
+            <path id="curve" fill="transparent" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+            <text width="500">
+                <textPath xlink:href="#curve">
+                Good Afternoon Karabo, Welcome to your Diary Entries
+                </textPath>
+            </text>
+         </svg>
+         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illum, possimus similique facere ab reiciendis alias dignissimos sed reprehenderit facilis temporibus quia minus, excepturi, aut officia delectus! Atque quod corrupti culpa magni repellat, enim assumenda harum tenetur totam exercitationem facere pariatur et nostrum recusandae. Obcaecati asperiores vero laboriosam accusantium esse.</h1>
+        </div>
 
     </div>
 
